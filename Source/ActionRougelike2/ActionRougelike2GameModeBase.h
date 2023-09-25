f7707b024b5ba2759fe03b88d74e2ea2c9bc4aa1
szywdwd <452979782@qq.com>
2023-09-25 22:21:11 +0800

    P2
    till to P2

---
+++ Source/ActionRougelike2/ActionRougelike2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRougelike2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROUGELIKE2_API AActionRougelike2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
